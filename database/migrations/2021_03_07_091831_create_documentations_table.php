eb776b600c939d45a5dd1879dab9f8d3c03d81e1
Harun Al Rosyid/170101010 <38546311+harun248@users.noreply.github.com>
2021-03-07 09:41:25 +0000

    create database migration and controller 07-03-21

---
+++ database/migrations/2021_03_07_091831_create_documentations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documentations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documentations');
+    }
+}
